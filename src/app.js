6f4490ac76a2d4563dc09f9d53d4395372166193
mahume <michael.a.hume@gmail.com>
2019-08-04 13:47:20 -0600

    Refactoring code

--- src/app.js
+++ src/app.js
@@ -1,38 +1,20 @@
 const todoList = {
   todos: [],
-  displayTodos() {
-    if (this.todos.length === 0) {
-      console.log('Your todo list is empty.');
-    } else {
-      console.log(`My todos:`);
-      for (let i = 0; i < this.todos.length; i++) {
-        if (this.todos[i].completed === true) {
-          console.log(`(x) ${this.todos[i].todoText}`);
-        } else {
-          console.log(`( ) ${this.todos[i].todoText}`);
-        }
-      }
-    }
-  },
   addTodo(todoText) {
     this.todos.push({
       todoText,
       completed: false,
     });
-    this.displayTodos();
   },
   changeTodo(index, todoText) {
     this.todos[index].todoText = todoText;
-    this.displayTodos();
   },
   deleteTodo(index) {
     this.todos.splice(index, 1);
-    this.displayTodos();
   },
   toggleCompleted(index) {
     const todo = this.todos[index];
     todo.completed = !todo.completed;
-    this.displayTodos();
   },
   toggleAll() {
     const totalTodos = this.todos.length;
@@ -51,41 +33,8 @@ const todoList = {
         this.todos[i].completed = true;
       }
     }
-    this.displayTodos();
   },
 };
-
-const handlers = {
-  displayTodos() {
-    todoList.displayTodos();
-  },
-  addTodo() {
-    const addTodoTextInput = document.getElementById('add-todo-text-input');
-    todoList.addTodo(addTodoTextInput.value);
-    addTodoTextInput.value = '';
-  },
-  changeTodo() {
-    const changeTodoIndexInput = document.getElementById('change-todo-index-input');
-    const changeTodoTextInput = document.getElementById('change-todo-text-input');
-    todoList.changeTodo(changeTodoIndexInput.valueAsNumber, changeTodoTextInput.value);
-    changeTodoIndexInput.value = '';
-    changeTodoTextInput.value = '';
-  },
-  deleteTodo() {
-    const deleteTodoIndexInput = document.getElementById('delete-todo-index-input');
-    todoList.deleteTodo(deleteTodoIndexInput.valueAsNumber);
-    deleteTodoIndexInput.value = '';
-  },
-  toggleCompleted() {
-    const toggleCompletedIndexInput = document.getElementById('toggle-completed-index-input');
-    todoList.toggleCompleted(toggleCompletedIndexInput.valueAsNumber);
-    toggleCompletedIndexInput.value = '';
-  },
-  toggleAll() {
-    todoList.toggleAll();
-  },
-};
-
 const view = {
   displayTodos() {
     const todosUl = document.querySelector('ul');
@@ -105,5 +54,37 @@ const view = {
       todoLi.textContent = todoTextWithCompletion;
       todosUl.appendChild(todoLi);
     }
+  },
+};
+const handlers = {
+  addTodo() {
+    const addTodoTextInput = document.getElementById('add-todo-text-input');
+    todoList.addTodo(addTodoTextInput.value);
+    addTodoTextInput.value = '';
+    view.displayTodos();
+  },
+  changeTodo() {
+    const changeTodoIndexInput = document.getElementById('change-todo-index-input');
+    const changeTodoTextInput = document.getElementById('change-todo-text-input');
+    todoList.changeTodo(changeTodoIndexInput.valueAsNumber, changeTodoTextInput.value);
+    changeTodoIndexInput.value = '';
+    changeTodoTextInput.value = '';
+    view.displayTodos();
+  },
+  deleteTodo() {
+    const deleteTodoIndexInput = document.getElementById('delete-todo-index-input');
+    todoList.deleteTodo(deleteTodoIndexInput.valueAsNumber);
+    deleteTodoIndexInput.value = '';
+    view.displayTodos();
+  },
+  toggleCompleted() {
+    const toggleCompletedIndexInput = document.getElementById('toggle-completed-index-input');
+    todoList.toggleCompleted(toggleCompletedIndexInput.valueAsNumber);
+    toggleCompletedIndexInput.value = '';
+    view.displayTodos();
+  },
+  toggleAll() {
+    todoList.toggleAll();
+    view.displayTodos();
   },
 };
